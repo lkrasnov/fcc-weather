fd219820ad72bfaaf05ef6abe02270a0e53348b0
Leo Krasnov <leonid.krasnov0@gmail.com>
2017-02-25 22:07:28 -0600

    Api call to openweather working, UI elements updating with data

--- js/weather.js
+++ js/weather.js
@@ -1,73 +1,99 @@
 $(document).ready(function() {
+  var tempKelvin = 0;
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
-      //$("#data").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
+      $.ajax({
+        cache: false,
+        url: "http://api.openweathermap.org/data/2.5/weather",
+        data: {
+          lat: position.coords.latitude,
+          lon: position.coords.longitude,
+          APPID: "c2b2331092dcd4ce4a98d60e9adbf234"
+        },
+        dataType: "json",
+        success: function(data) {
+          updateWeatherIcon(data.weather[0].icon);
+          tempKelvin = data.main.temp;
+          updateTempFahrenheit(tempKelvin);
+          updateLocation(data.name);
+        }
+      });
     });
   }
 
-  $("#getQuote").on("click", function() {
-    $.ajax({
-      cache: false,
-      url: "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",
-      dataType: "json",
-      success: function(data) {
-        var quote = data[0];
-        var quoteContent = quote.content.slice(3, quote.content.length - 5);
-        var quoteTitle = quote.title;
+  $("#fahrenheit").on("click", function() {
+    updateTempFahrenheit(tempKelvin);
+  });
 
-        $(".quote-content").html(quoteContent);
-        $(".quote-title").html("— " + quoteTitle.toUpperCase());
-        $(".quote").removeClass("hide"); 
-        $("#tweet").attr("href", "http://twitter.com/home?status=" + quoteContent + " — " + quoteTitle);
-      }
-    });
+  $("#celcius").on("click", function() {
+    updateTempCelcius(tempKelvin);
   });
 
 });
 
-function getWeather() {
-  return {
-    "coord": {
-      "lon": -87.73,
-      "lat": 42
-    },
-    "weather": [{
-      "id": 800,
-      "main": "Clear",
-      "description": "clear sky",
-      "icon": "01n"
-    }],
-    "base": "stations",
-    "main": {
-      "temp": 265.379,
-      "pressure": 1017.61,
-      "humidity": 100,
-      "temp_min": 265.379,
-      "temp_max": 265.379,
-      "sea_level": 1042.07,
-      "grnd_level": 1017.61
-    },
-    "wind": {
-      "speed": 4.35,
-      "deg": 248.001
-    },
-    "clouds": {
-      "all": 0
-    },
-    "dt": 1486695892,
-    "sys": {
-      "message": 0.0073,
-      "country": "US",
-      "sunrise": 1486731171,
-      "sunset": 1486768680
-    },
-    "id": 4899988,
-    "name": "Lincolnwood",
-    "cod": 200
-  };
+function updateLocation(location) {
+  $("#location").html(location);
 }
 
-function displayWeather(weather) {
+function updateTempFahrenheit(kelvin) {
+  var fahrenheit = (9 / 5) * (kelvin - 273) + 32;
+  $("#temp").html(Math.round(fahrenheit) + "&deg; F");
+}
 
+function updateTempCelcius(kelvin) {
+  var celcius = kelvin - 273;
+  $("#temp").html(Math.round(celcius) + "&deg; C");
+}
+
+function updateWeatherIcon(iconCode) {
+  var iconCss = "wi ";
+  switch (iconCode) {
+    case "01d":
+      iconCss += "wi-day-sunny";
+      break;
+    case "01n":
+      iconCss += "wi-night-clear";
+      break;
+    case "02d":
+      iconCss += "wi-day-cloudy";
+      break;
+    case "02n":
+      iconCss += "wi-night-alt-cloudy";
+      break;
+    case "03d":
+    case "03n":
+      iconCss += "wi-cloud";
+      break;
+    case "04d":
+    case "04n":
+      iconCss += "wi-cloudy";
+      break;
+    case "09d":
+    case "09n":
+      iconCss += "wi-showers";
+      break;
+    case "10d":
+      iconCss += "wi-day-rain";
+      break;
+    case "10n":
+      iconCss += "wi-night-alt-rain";
+      break;
+    case "11d":
+    case "11n":
+      iconCss += "wi-thunderstorm";
+      break;
+    case "13d":
+    case "13n":
+      iconCss += "wi-snow";
+      break;
+    case "50d":
+    case "50n":
+      iconCss += "wi-fog";
+      break;
+    default:
+      iconCss += "wi-alien";
+  };
+
+  $(".icon-weather").html('<i class="' + iconCss + '"></i>');
 }
